59877e0d5aa8d82e6d86a31acb8a27b2e748c23b
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-07-12 21:32:48 -0300

    atual

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 'use strict'/*Para combater deficiências, você pode ativar o modo estrito, que reduz erros silenciosos, melhora o desempenho, fornece mais avisos e menos recursos não seguros.*/
 const switcher = document.querySelector('.btn');
+
 switcher.addEventListener('click', function(){
   document.body.classList.toggle('dark-theme')
-});
 
-var className = document.body.className;
-if(className == "dark-theme"){
-  this.textContent = "light";
-}
-else{
-  this.texrContent = "dark-theme";
-}
+   var className = document.body.className;
+   if(className == "light-theme"){
+       this.textContent = "Dark";
+     }
+       else{
+       this.texrContent = "Light";
+       }
+       console.log('current class name: ' + className);
+         });
