3bb7ad37cad113cea3adacec4ee9bea1c78db01b
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-07-24 13:16:28 -0300

    atual

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-'use strict'/*Para combater deficiências, você pode ativar o modo estrito, que reduz erros silenciosos, melhora o desempenho, fornece mais avisos e menos recursos não seguros.*/
-const switcher = document.querySelector('.btn');
+'use strict' /*Para combater deficiências, você pode ativar o modo estrito, que reduz erros silenciosos, melhora o desempenho, fornece mais avisos e menos recursos não seguros.*/
+const switcher = document.querySelector('.btn')
 
-switcher.addEventListener('click', function(){
-  document.body.classList.toggle('dark-theme')
-
-  var className = document.body.className;
-  if(className == "light-theme"){
-    this.textContent = "Dark";
+switcher.addEventListener('click', function () {
+  var className = document.body.className
+  if (className == 'light-theme') {
+    document.body.classList.remove('light-theme')
+    document.body.classList.add('dark-theme')
+    this.textContent = 'Dark'
+  } else {
+    this.textContent = 'Light'
+    document.body.classList.remove('dark-theme')
+    document.body.classList.add('light-theme')
   }
-  else{
-    this.textContent = "Light";
-  }
-  console.log('current class name: ' + className);
-});
+  console.log('current class name: ' + className)
+})
